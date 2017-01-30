3190c891ed0f8091f4639dd625bc63bc4c0909e7
KawaiiBot <kawaiibot@kakusu.animemusic.me>
2017-01-30 22:23:54 +0100

    Verify signature and pass errors back to github

--- poke.php
+++ poke.php
@@ -1,1 +1,16 @@
-<?php echo `git pull 2>&1`;
+<?php
+	$secret = trim(file_get_contents('../.github-secret'));
+	$data = file_get_contents('php://input');
+	list($algo, $hash) = explode('=', $_SERVER['HTTP_X_HUB_SIGNATURE'], 2) + array('', '');
+	if($hash != hash_hmac($algo, $data, $secret))
+	{
+		header('HTTP/1.0 403 Forbidden');
+		die('Invalid secret');
+	}
+	$out = [];
+	$ret = 0;
+	exec('git pull 2>&1', $out, $ret)
+	if($ret != 0)
+		header('HTTP/1.1 500 Internal error');
+	echo join("\n", $out);
+
